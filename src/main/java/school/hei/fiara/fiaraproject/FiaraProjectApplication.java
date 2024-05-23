2f5d87bae25df8086b87c2f65ad7b787d9b7db9e
josiaJj <hei.josia@gmail.com>
2024-05-23 22:08:17 +0300

    init: environment generated

---
+++ src/main/java/school/hei/fiara/fiaraproject/FiaraProjectApplication.java
@@ -0,0 +1,13 @@
+package school.hei.fiara.fiaraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiaraProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiaraProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/fiara/fiaraproject/FiaraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.fiara.fiaraproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiaraProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
